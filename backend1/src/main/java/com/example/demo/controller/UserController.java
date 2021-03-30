cebb63937f9db5aacc7e06d73cf685bcd0a75b93
rohith-poreddy <rohithreddyporeddy@gmail.com>
2021-03-30 12:36:08 +0530

    commit3

--- backend1/src/main/java/com/example/demo/controller/UserController.java
+++ backend1/src/main/java/com/example/demo/controller/UserController.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
-@RequestMapping("api/")
 public class UserController {
   @Autowired
   private UserRepository userRepository;
@@ -21,4 +23,20 @@ public class UserController {
 	  return this.userRepository.findAll();
 	  
   }
+  
+  
+  @PostMapping("signup")
+  public boolean getUser(@RequestBody User user)
+  {
+	  String email=user.getEmail();
+	  int size=(userRepository.findByEmail(email)).size();
+	  if(size==0)
+	  {
+	  this.userRepository.save(user);
+	  return true;
+	  }
+	  return false;
+	  
+  }
+  
 }
--- backend1/src/main/java/com/example/demo/repository/UserRepository.java
+++ backend1/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
-
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.User;
+
+
 @Repository
 public interface UserRepository extends CrudRepository<User,Long>,JpaRepository<User,Long> {
- 
+    public List<User> findByEmail(String email);
 }
